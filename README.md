35450b940c0be8c17a92b92045e7a8aa80e56d7e
codesteller <pallab.vsp@gmail.com>
2023-01-29 00:01:38 +0530

    added multiprocessing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# waymo-dataset-tool
-This a repo to download and extract Waymo Dataset
+# Credit
+The download and extract module of this repository is based on the work of 
+[Huizi Mao's](!https://github.com/RalphMao) [Waymo Open Dataset](!https://github.com/codesteller/waymo-dataset-tool)
+
+# Waymo Dataset Toolset
+This a repo to download and extract Waymo Dataset. This tool downloads Waymo Open Dataset and converts it into KITTI-like format that consists of individual images and txt labels. It also loads the Waymo Dataset into a tf.data.Dataset object and/or Pytorch Dataset object.
+
+# Pre-requisites
+- [Google Cloud SDK](!https://cloud.google.com/sdk/docs/install-sdk)
+- Waymo Open Dataset Account Sign Up [here](!https://waymo.com/open/)
+
---
+++ batch_download.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import multiprocessing
+import numpy as np
+import tools.waymo as wod
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Download and convert Waymo dataset to KITTI format')
+    parser.add_argument('--out_dir', type=str, default='./waymo', help='Output directory')
+    parser.add_argument('--split', type=str, default='training', help='training or validation')
+    parser.add_argument('--resize', type=float, default=1, help='Resize ratio')
+    args = parser.parse_args()
+
+    if args.split == 'training':
+        num_segs = 32
+    elif args.split == 'validation':
+        num_segs = 8
+    else:
+        raise ValueError('Invalid split: %s'%args.split)
+
+    os.makedirs(args.out_dir, exist_ok=True)
+
+    labels_dir = os.path.join(args.out_dir, 'labels')
+    images_dir = os.path.join(args.out_dir, 'images')
+    os.makedirs(labels_dir, exist_ok=True)
+    os.makedirs(images_dir, exist_ok=True)
+
+    seg_ids = np.random.choice(100, args.num_seg, replace=False)
+    seg_ids.sort()
+    for seg_id in seg_ids:
+        wod.download_and_convert(args, seg_id, labels_dir, images_dir)
+
+    print("Done")
+
+
+
+if __name__ == '__main__':
+    main()
